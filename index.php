b43e148c741259ed6571bdced8b702b5fde1a528
Nabil Kashyap <n@nabilk.com>
2013-08-01 04:42:42 -0400

    table of contents and scrollspy

--- index.php
+++ index.php
@@ -9,21 +9,34 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="css/md_web_style.css" rel="stylesheet" media="all">
   </head>
-  <body>
+  <body data-spy="scroll" data-target="#sidenav">
 
-    <div class="col-lg-8 col-offset-2">
+    <div class="container">
+        <div class="row">
 
-    <?php     
-    include_once 'php-markdown-lib/Markdown.php';
-    include_once 'php-markdown-lib/MarkdownExtra.php';
-    use \Michelf\Markdown;
-    use \Michelf\MarkdownExtra;
-    $out = nl2br(file_get_contents('md_guidelines.md'));
-    $out = MarkdownExtra::defaultTransform($out);
-    echo $out;
-    ?>
+            <div id="sidenav" class="col-lg-3  affix">
 
+                    <ul class="nav sidenav">
+                    </ul>
+
+            </div>
+            
+            <div class="col-lg-9 col-offset-3">
+
+            <?php     
+                include_once 'php-markdown-lib/Markdown.php';
+                include_once 'php-markdown-lib/MarkdownExtra.php';
+                use \Michelf\Markdown;
+                use \Michelf\MarkdownExtra;
+                $out = nl2br(file_get_contents('md_guidelines.md'));
+                $out = MarkdownExtra::defaultTransform($out);
+                echo $out;
+            ?>
+
+            </div>
+        </div>
     </div>
+
     <!-- JavaScript plugins (requires jQuery) -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -32,5 +45,6 @@
     <!-- Optionally enable responsive features in IE8 -->
     <script src="js/respond.js"></script>
     <script src="js/main.js"></script>
+    <script type="text/javascript">  $('#sidenav').scrollspy(); </script>
   </body>
 </html>
